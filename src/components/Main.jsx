097dff48374fe70ecbc36872f27bf74a28c9d002
yuliia volkova <yuliia_volkova@icloud.com>
2025-05-14 18:52:28 +0200

    created state for top, bottom, image in Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,12 @@
+import React from "react";
+
 export default function Main() {
+  const [meme, setMeme] = React.useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+
   return (
     <main>
       <div className="form">
@@ -14,9 +22,9 @@ export default function Main() {
         <button>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
-        <img src="http://i.imgflip.com/1bij.jpg" />
-        <span className="top">One does not simply</span>
-        <span className="bottom">Walk into Mordor</span>
+        <img src={meme.imageUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
       </div>
     </main>
   );
